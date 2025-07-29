0418b67e4b8322b1906f0e1a0c03f1bebb0df440
Luis Miguel <87282690+rhaastt@users.noreply.github.com>
2025-07-29 11:29:47 +0000

    passando raiva com o tailwind

---
+++ my-project/src/App.tsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-600">teste</h1>
+        <Form />
+      </div>
+    </>
+  );
+}
+
+export default App;
